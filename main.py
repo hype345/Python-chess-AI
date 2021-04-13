f8ac91f641e4c49293d4c8ab1d70a867c662f8ae
Chase Shriver <chase802@icloud.com>
2021-04-13 02:57:08 -0400

    a lot

--- main.py
+++ main.py
@@ -16,64 +16,64 @@ board = chess.Board()
 
 # Evaluating the board
 pawntable = [
-    0, 0, 0, 0, 0, 0, 0, 0,
-    5, 10, 10, -20, -20, 10, 10, 5,
-    5, -5, -10, 0, 0, -10, -5, 5,
-    0, 0, 0, 20, 20, 0, 0, 0,
-    5, 5, 10, 25, 25, 10, 5, 5,
-    10, 10, 20, 30, 30, 20, 10, 10,
-    50, 50, 50, 50, 50, 50, 50, 50,
-    0, 0, 0, 0, 0, 0, 0, 0]
+    0,   0,   0,   0,   0,   0,   0,   0,
+    78,  83,  86,  73, 102,  82,  85,  90,
+    7,  29,  21,  44,  40,  31,  44,   7,
+    -17,  16,  -2,  15,  14,   0,  15, -13,
+    -26,   3,  10,   9,   6,   1,   0, -23,
+    -22,   9,   5, -11, -10,  -2,   3, -19,
+    -31,   8,  -7, -37, -36, -14,   3, -31,
+    0,   0,   0,   0,   0,   0,   0,   0]
 
 knightstable = [
-    -50, -40, -30, -30, -30, -30, -40, -50,
-    -40, -20, 0, 5, 5, 0, -20, -40,
-    -30, 5, 10, 15, 15, 10, 5, -30,
-    -30, 0, 15, 20, 20, 15, 0, -30,
-    -30, 5, 15, 20, 20, 15, 5, -30,
-    -30, 0, 10, 15, 15, 10, 0, -30,
-    -40, -20, 0, 0, 0, 0, -20, -40,
-    -50, -40, -30, -30, -30, -30, -40, -50]
+    -66, -53, -75, -75, -10, -55, -58, -70,
+    -3,  -6, 100, -36,   4,  62,  -4, -14,
+    10,  67,   1,  74,  73,  27,  62,  -2,
+    24,  24,  45,  37,  33,  41,  25,  17,
+    -1,   5,  31,  21,  22,  35,   2,   0,
+    -18,  10,  13,  22,  18,  15,  11, -14,
+    -23, -15,   2,   0,   2,   0, -23, -20,
+    -74, -23, -26, -24, -19, -35, -22, -69]
 
 bishopstable = [
-    -20, -10, -10, -10, -10, -10, -10, -20,
-    -10, 5, 0, 0, 0, 0, 5, -10,
-    -10, 10, 10, 10, 10, 10, 10, -10,
-    -10, 0, 10, 10, 10, 10, 0, -10,
-    -10, 5, 5, 10, 10, 5, 5, -10,
-    -10, 0, 5, 10, 10, 5, 0, -10,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -20, -10, -10, -10, -10, -10, -10, -20]
+    -59, -78, -82, -76, -23,-107, -37, -50,
+    -11,  20,  35, -42, -39,  31,   2, -22,
+    -9,  39, -32,  41,  52, -10,  28, -14,
+    25,  17,  20,  34,  26,  25,  15,  10,
+    13,  10,  17,  23,  17,  16,   0,   7,
+    14,  25,  24,  15,   8,  25,  20,  15,
+    19,  20,  11,   6,   7,   6,  20,  16,
+    -7,   2, -15, -12, -14, -15, -10, -10]
 
 rookstable = [
-    0, 0, 0, 5, 5, 0, 0, 0,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    5, 10, 10, 10, 10, 10, 10, 5,
-    0, 0, 0, 0, 0, 0, 0, 0]
+    35,  29,  33,   4,  37,  33,  56,  50,
+    55,  29,  56,  67,  55,  62,  34,  60,
+    19,  35,  28,  33,  45,  27,  25,  15,
+    0,   5,  16,  13,  18,  -4,  -9,  -6,
+    -28, -35, -16, -21, -13, -29, -46, -30,
+    -42, -28, -42, -25, -25, -35, -26, -46,
+    -53, -38, -31, -26, -29, -43, -44, -53,
+    -30, -24, -18,   5,  -2, -18, -31, -32]
 
 queenstable = [
-    -20, -10, -10, -5, -5, -10, -10, -20,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -10, 5, 5, 5, 5, 5, 0, -10,
-    0, 0, 5, 5, 5, 5, 0, -5,
-    -5, 0, 5, 5, 5, 5, 0, -5,
-    -10, 0, 5, 5, 5, 5, 0, -10,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -20, -10, -10, -5, -5, -10, -10, -20]
+    6,   1,  -8,-104,  69,  24,  88,  26,
+    14,  32,  60, -10,  20,  76,  57,  24,
+    -2,  43,  32,  60,  72,  63,  43,   2,
+    1, -16,  22,  17,  25,  20, -13,  -6,
+    -14, -15,  -2,  -5,  -1, -10, -20, -22,
+    -30,  -6, -13, -11, -16, -11, -16, -27,
+    -36, -18,   0, -19, -15, -15, -21, -38,
+    -39, -30, -31, -13, -31, -36, -34, -42]
 
 kingstable = [
-    20, 30, 10, 0, 0, 10, 30, 20,
-    20, 20, 0, 0, 0, 0, 20, 20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30]
+    4,  54,  47, -99, -99,  60,  83, -62,
+    -32,  10,  55,  56,  56,  55,  10,   3,
+    -62,  12, -57,  44, -67,  28,  37, -31,
+    -55,  50,  11,  -4, -19,  13,   0, -49,
+    -55, -43, -52, -28, -51, -47,  -8, -50,
+    -47, -42, -43, -79, -64, -32, -29, -32,
+    -4,   3, -14, -50, -57, -18,  13,   4,
+    17,  30,  -3, -14,   6,  -1,  40,  18]
 
 
 def evaluate_board():
@@ -102,6 +102,7 @@ def evaluate_board():
                     elif tablebase.probe_wdl(board) == 0:
                         return 0
         except:
+            print(num_peices(), board.fen())
             print('error position not found in table base though it should be there')
 
     wp = len(board.pieces(chess.PAWN, chess.WHITE))
@@ -115,7 +116,7 @@ def evaluate_board():
     wq = len(board.pieces(chess.QUEEN, chess.WHITE))
     bq = len(board.pieces(chess.QUEEN, chess.BLACK))
 
-    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+    material = 100 * (wp - bp) + 280 * (wn - bn) + 320 * (wb - bb) + 479 * (wr - br) + 929 * (wq - bq)
 
     pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
     pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
@@ -137,10 +138,7 @@ def evaluate_board():
                         for i in board.pieces(chess.KING, chess.BLACK)])
 
     eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
-    if board.turn:
-        return eval
-    else:
-        return -eval
+    return eval
 
 # find number of piece left on board to see if we should look at tablebases
 def num_peices():
@@ -178,16 +176,35 @@ def num_peices():
 
     return total
 
+# Orders moves to speed up search through better prunning
+def moveOrdering(moves):
+    result = {}
+    pieceVaules = {1: 1, 2: 3, 3: 3, 4: 5, 5: 9, 6: 0}
+    for move in moves:
+        moveSore = 0
+        if board.is_capture(move):
+            if board.is_en_passant(move):
+                moveSore += 20
+            else:
+                moveSore += (10 * (pieceVaules[board.piece_at(move.to_square).piece_type] - pieceVaules[board.piece_at(move.from_square).piece_type])) + 5
+        if move.promotion is not None:
+            moveSore += pieceVaules[move.promotion] * 7
+        result[move] = moveSore
+    result = sorted(result.items(), key=lambda x: x[1], reverse=True)
+    finalResult = []
+    for obj in result:
+        finalResult.append(obj[0])
+    return finalResult
+
 
 # Searching the best move using minimax and alphabeta algorithm with negamax implementation
 def alphabeta(alpha, beta, depthleft):
-    # global positionsSearched
     bestscore = -9999
     if (depthleft == 0):
         return quiesce(alpha, beta)
-    for move in board.legal_moves:
+    moves = moveOrdering(board.legal_moves)
+    for move in moves:
         board.push(move)
-        # positionsSearched += 1
         score = -alphabeta(-beta, -alpha, depthleft - 1)
         board.pop()
         if (score >= beta):
@@ -198,19 +215,21 @@ def alphabeta(alpha, beta, depthleft):
             alpha = score
     return bestscore
 
-
+# Before evauling board finds quite possition (No captures)
 def quiesce(alpha, beta):
-    # global positionsSearched
-    stand_pat = evaluate_board()
+    if board.turn:
+        stand_pat = evaluate_board()
+    else:
+        stand_pat = -(evaluate_board())
+        
     if (stand_pat >= beta):
         return beta
     if (alpha < stand_pat):
         alpha = stand_pat
-
-    for move in board.legal_moves:
+    moves = moveOrdering(board.legal_moves)
+    for move in moves:
         if board.is_capture(move):
             board.push(move)
-            # positionsSearched += 1
             score = -quiesce(-beta, -alpha)
             board.pop()
 
@@ -222,11 +241,8 @@ def quiesce(alpha, beta):
 
 
 def selectmove(depth):
-    # global positionsSearched
     try:
         move = chess.polyglot.MemoryMappedReader("/Users/cshriver/Desktop/Chess-python/books/human.bin").weighted_choice(board).move
-        # move = chess.polyglot.MemoryMappedReader("/Users/cshriver/Desktop/Chess-python/books/computer.bin").weighted_choice(board).move
-        # move = chess.polyglot.MemoryMappedReader("/Users/cshriver/Desktop/Chess-python/books/human.bin").weighted_choice(board).move
         return move
     except:
         if num_peices() <= 5:
@@ -237,7 +253,6 @@ def selectmove(depth):
                     bestMove = chess.Move.null()
                     for move in board.legal_moves:
                         board.push(move)
-                        # positionsSearched += 1
                         if cur_dtz > 0:
                             if tablebase.probe_dtz(board) < cur_dtz and tablebase.probe_dtz(board) < 0:
                                 if fastest_win < tablebase.probe_dtz(board):
@@ -258,9 +273,9 @@ def selectmove(depth):
         bestValue = -99999
         alpha = -100000
         beta = 100000
-        for move in board.legal_moves:
+        moves = moveOrdering(board.legal_moves)
+        for move in moves:
             board.push(move)
-            # positionsSearched += 1
             boardValue = -alphabeta(-beta, -alpha, depth - 1)
             if boardValue > bestValue:
                 bestValue = boardValue
@@ -274,7 +289,7 @@ def selectmove(depth):
 # Searching alphafish's Move
 def alphafish_move():
     if not board.is_game_over(claim_draw=True):
-        move = selectmove(3)
+        move = selectmove(4)
         moveHistory.append(str(move))
         board.push(move)
     else:
@@ -341,7 +356,6 @@ def store_FEN(fen):
 
 
 app = Flask(__name__)
-# app.config['DEBUG'] = True
 
 
 # Front Page of the Flask Web Page
@@ -356,7 +370,6 @@ def main():
     ret += '<img width=410 height=410 src="/board.svg?%f"></img></br>' % time.time()
     ret +=  str(moveHistory)
     ret += '</br>'
-    # ret += str(positionsSearched)
     ret += '<form action="/game/" method="post"><button name="New Game" type="submit">New Game</button></form>'
     ret += '<form action="/load/"><input type="submit" value="Load FEN:"><input name="FEN" type="text"></input></form>'
     ret += '<form action="/undo/" method="post"><button name="Undo" type="submit">Undo Last Move</button></form>'
@@ -405,8 +418,6 @@ def recv():
 @app.route("/alphafish/", methods=['POST'])
 def alphafish():
     try:
-        # global positionsSearched
-        # positionsSearched = 0
         alphafish_move()
         set_result()
     except Exception:
